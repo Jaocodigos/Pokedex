5137f31e576096349f435d7ab9092dac3b63ebac
Jaocodigos <jovicode@gmail.com>
2022-08-08 18:01:47 -0300

    Frontend #2

--- app/__init__.py
+++ app/__init__.py
@@ -8,25 +8,28 @@ app.secret_key = 'thebestofthebests'
 
 @app.route('/')
 def index():
-    return render_template('index.html', titulo='Pokedex 1.0')
+    return render_template('index.html', titulo='Pokedex v0.2', dex=url_for('static', filename='pokeball.png'))
 
 
-@app.route('/buscar', methods=['POST', ])
+@app.route('/dex', methods=['POST', ])
 def search():
     nome = str(request.form['poke'])
 
     buscar = f'https://pokeapi.co/api/v2/pokemon/{nome}'
     res = requests.get(buscar)
-    pokemon = res.json()
-    id = pokemon['id']
-    deximg = f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png'
-    type = []
-    habs = []
-    for abi in pokemon['abilities']:
-        habs.append(abi['ability']['name'])
-    for tip in pokemon['types']:
-        type.append(tip['type']['name'])
-    return render_template('index.html', titulo='Pokedex', type=type, habs=habs, dex=deximg)
+    if res:
+        pokemon = res.json()
+        id = pokemon['id']
+        deximg = f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png'
+        type = []
+        habs = []
+        for abi in pokemon['abilities']:
+            habs.append(abi['ability']['name'])
+        for tip in pokemon['types']:
+            type.append(tip['type']['name'])
+        return render_template('index.html', titulo='Pokedex v0.2', type=type, habs=habs, dex=deximg)
+    flash("O pokémon digitado não existe!")
+    return render_template('index.html', titulo='Pokedex v0.2')
 
 
 if __name__ == '__main__':
