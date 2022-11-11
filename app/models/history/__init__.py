68b913b8540c95c06da15c7989295cda9ca67ffe
Jaocodigos <jovicode@gmail.com>
2022-11-11 20:45:31 -0300

    Creating history model and schema

--- app/models/history/__init__.py
+++ app/models/history/__init__.py
@@ -5,4 +5,6 @@ class History(db.model):
     __tablename__ = 'history'
 
     id = db.Column(db.Integer, auto_increment=True, primary_key=True)
-    dex = db.Column(db.String(50), nullable=False, unique=False)
+    dex = db.Column(db.String(30), nullable=False, unique=False)
+    name = db.Column(db.String(30), nullable=False, unique=False)
+    type = db.Column(db.String(20), nullable=False, unique=False)
