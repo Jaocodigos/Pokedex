44effe2a4d91c27c4eeff812019dace194759d24
Jaocodigos <jovicode@gmail.com>
2023-01-28 21:29:03 -0300

    Added migrations and deleted feedback page

--- app/resources/feedback/feedback.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, session, flash, url_for, request, redirect
-from app.resources import poke
-import requests
-
-
-@poke.route('/feedback', methods=['GET'])
-def feedback():
-    return render_template('/pages/feedback.html')
